4af8c5a8004f96f2e71961ad0f061f315c9a8210
webtech88 <aronszell88@gmail.com>
2020-01-30 06:35:26 +0300

    confirm to delete using modal

--- app/components/invoices-form.js
+++ app/components/invoices-form.js
@@ -3,10 +3,13 @@ import Component from '@ember/component';
 export default Component.extend({
     tagName: '',
     newDate: new Date().toISOString().slice(0, 10),
-    isShowingModalDialog: false,
-    selectedIndex: 0,
+    isShowingUpdateModalDialog: false,
+    isShowingDeleteConfirmModalDialog: false,
+    selectedIndex: -1,
     selectedInvoiceDueDate: "",
     selectedInvoiceAmount: "",
+    deletingInvoiceIndex: -1,
+    deletingInvoiceNumber: "",
     invoices: [
         { dueDate: '2020/01/30', num: '27478399374', amount: '1500' },
         { dueDate: '2020/01/30', num: '29937474783', amount: '2500' },
@@ -14,7 +17,7 @@ export default Component.extend({
     ],
     actions: {
         addInvoice: function () {
-            if (this.get("isShowingModalDialog"))
+            if (this.get("isShowingUpdateModalDialog") || this.get("isShowingDeleteConfirmModalDialog"))
                 return;
             let invoiceNumber = (Math.floor(Math.random() * (99999999999 - 10000000000)) + 10000000000).toString() + "";
             let invoiceAmount = this.get('invoiceAmount');
@@ -39,22 +42,30 @@ export default Component.extend({
             d = d.slice(d.length - 2, d.length);
             currentInvoice = { dueDate: y + "/" + m + "/" + d, num: currentInvoice.num, amount: this.get("selectedInvoiceAmount") }
             this.get("invoices").replace(index, 1, [currentInvoice]);
-            this.set('isShowingModalDialog', false);
+            this.set('isShowingUpdateModalDialog', false);
         },
         deleteInvoice: function (index) {
-            if (this.get("isShowingModalDialog"))
+            if (this.get("isShowingUpdateModalDialog"))
                 return;
-            this.get('invoices').removeAt(index)
+            this.set("deletingInvoiceNumber", this.get("invoices").objectAt(index).num);
+            this.set("isShowingDeleteConfirmModalDialog", true);
+            this.set("deletingInvoiceIndex", index);
+        },
+        confirmDelete: function () {
+            this.get('invoices').removeAt(this.get("deletingInvoiceIndex"));
+            this.set("isShowingDeleteConfirmModalDialog", false);
         },
         openUpdateDialog(index) {
+            if (this.get("isShowingDeleteConfirmModalDialog"))
+                return;
             this.set("selectedIndex", index);
             let invoice = this.get("invoices").objectAt(index);
             this.set("selectedInvoiceDueDate", invoice.dueDate);
             this.set("selectedInvoiceAmount", invoice.amount);
-            this.set('isShowingModalDialog', true);
+            this.set('isShowingUpdateModalDialog', true);
         },
         closeModalDialog() {
-            this.set('isShowingModalDialog', false);
+            this.set('isShowingUpdateModalDialog', false);
         },
         openDatePicker: function () {
             // this.set("newDate", new Date().toISOString().slice(0, 10));
