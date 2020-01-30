2deae8415c8e1c9c3902823f7eeb4b97395ecd84
webtech88 <aronszell88@gmail.com>
2020-01-30 04:59:14 +0300

    param modification

--- app/components/invoices-form.js
+++ app/components/invoices-form.js
@@ -5,35 +5,39 @@ export default Component.extend({
     isShowingModalDialog: false,
     selectedIndex: 0,
     selectedInvoiceNum: "",
-    selectedInvoicePrice: "",
+    selectedInvoiceAmount: "",
     invoices: [
-        { num: '1', price: '1500' },
-        { num: '2', price: '2000' },
-        { num: '3', price: '3000' }
+        { num: '1', amount: '1500' },
+        { num: '2', amount: '2000' },
+        { num: '3', amount: '3000' }
     ],
     actions: {
         addInvoice: function () {
+            if (this.get("isShowingModalDialog"))
+                return;
             let invoiceNumber = this.get('invoiceNumber');
-            let invoicePrice = this.get('invoicePrice');
-            this.get('invoices').pushObject({ num: invoiceNumber, price: invoicePrice })
+            let invoiceAmount = this.get('invoiceAmount');
+            this.get('invoices').pushObject({ num: invoiceNumber, amount: invoiceAmount })
         },
         updateInvoice: function () {
             let index = this.get("selectedIndex");
             let currentInvoice = this.get("invoices").objectAt(index);
             console.log("current num: ", currentInvoice.num);
-            console.log("current price: ", currentInvoice.price);
-            currentInvoice = { num: this.get("selectedInvoiceNum"), price: this.get("selectedInvoicePrice") }
+            console.log("current amount: ", currentInvoice.amount);
+            currentInvoice = { num: this.get("selectedInvoiceNum"), amount: this.get("selectedInvoiceAmount") }
             this.get("invoices").replace(index, 1, [currentInvoice]);
             this.set('isShowingModalDialog', false);
         },
         deleteInvoice: function (index) {
+            if (this.get("isShowingModalDialog"))
+                return;
             this.get('invoices').removeAt(index)
         },
         openModalDialog(index) {
             this.set("selectedIndex", index);
             let invoice = this.get("invoices").objectAt(index);
             this.set("selectedInvoiceNum", invoice.num);
-            this.set("selectedInvoicePrice", invoice.price);
+            this.set("selectedInvoiceAmount", invoice.amount);
             this.set('isShowingModalDialog', true);
         },
         closeModalDialog() {
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,15 +1,15 @@
 import Controller from '@ember/controller';
 export default Controller.extend({
     invoices: [
-        {num: '1', price: '1500'}, 
-        {num: '2', price: '2000'}, 
-        {num: '3', price: '3000'}
+        {num: '1', amount: '1500'}, 
+        {num: '2', amount: '2000'}, 
+        {num: '3', amount: '3000'}
     ],
     actions: {
         addInvoice: function(){
             let invoiceNumber = this.get('invoiceNumber');
-            let invoicePrice = this.get('invoicePrice');
-            this.get('invoices').pushObject({num: invoiceNumber, price: invoicePrice})
+            let invoiceAmount = this.get('invoiceAmount');
+            this.get('invoices').pushObject({num: invoiceNumber, amount: invoiceAmount})
         },
         deleteInvoice: function(index){
             this.get('invoices').splice(index, 1)
