751abef967d97418f55e50dd54b06e67ba81cb8a
webtech88 <aronszell88@gmail.com>
2020-01-30 06:17:32 +0300

    add and update date

--- app/components/invoices-form.js
+++ app/components/invoices-form.js
@@ -2,29 +2,42 @@ import Component from '@ember/component';
 
 export default Component.extend({
     tagName: '',
+    newDate: new Date().toISOString().slice(0, 10),
     isShowingModalDialog: false,
     selectedIndex: 0,
-    selectedInvoiceNum: "",
+    selectedInvoiceDueDate: "",
     selectedInvoiceAmount: "",
     invoices: [
-        { num: '1', amount: '1500' },
-        { num: '2', amount: '2000' },
-        { num: '3', amount: '3000' }
+        { dueDate: '2020/01/30', num: '27478399374', amount: '1500' },
+        { dueDate: '2020/01/30', num: '29937474783', amount: '2500' },
+        { dueDate: '2020/01/30', num: '27783993474', amount: '3500' },
     ],
     actions: {
         addInvoice: function () {
             if (this.get("isShowingModalDialog"))
                 return;
-            let invoiceNumber = this.get('invoiceNumber');
+            let invoiceNumber = (Math.floor(Math.random() * (99999999999 - 10000000000)) + 10000000000).toString() + "";
             let invoiceAmount = this.get('invoiceAmount');
-            this.get('invoices').pushObject({ num: invoiceNumber, amount: invoiceAmount })
+            let selectedDate = new Date(this.get("newDate"));
+            let y = "0000" + selectedDate.getFullYear().toString();
+            y = y.slice(y.length - 4, y.length);
+            let m = "00" + (selectedDate.getMonth() + 1).toString();
+            m = m.slice(m.length - 2, m.length);
+            let d = "00" + selectedDate.getDate().toString();
+            d = d.slice(d.length - 2, d.length);
+            this.get('invoices').pushObject({ dueDate: y + "/" + m + "/" + d, num: invoiceNumber, amount: invoiceAmount })
         },
         updateInvoice: function () {
             let index = this.get("selectedIndex");
             let currentInvoice = this.get("invoices").objectAt(index);
-            console.log("current num: ", currentInvoice.num);
-            console.log("current amount: ", currentInvoice.amount);
-            currentInvoice = { num: this.get("selectedInvoiceNum"), amount: this.get("selectedInvoiceAmount") }
+            let selectedDate = new Date(this.get("selectedInvoiceDueDate"));
+            let y = "0000" + selectedDate.getFullYear().toString();
+            y = y.slice(y.length - 4, y.length);
+            let m = "00" + (selectedDate.getMonth() + 1).toString();
+            m = m.slice(m.length - 2, m.length);
+            let d = "00" + selectedDate.getDate().toString();
+            d = d.slice(d.length - 2, d.length);
+            currentInvoice = { dueDate: y + "/" + m + "/" + d, num: currentInvoice.num, amount: this.get("selectedInvoiceAmount") }
             this.get("invoices").replace(index, 1, [currentInvoice]);
             this.set('isShowingModalDialog', false);
         },
@@ -33,15 +46,18 @@ export default Component.extend({
                 return;
             this.get('invoices').removeAt(index)
         },
-        openModalDialog(index) {
+        openUpdateDialog(index) {
             this.set("selectedIndex", index);
             let invoice = this.get("invoices").objectAt(index);
-            this.set("selectedInvoiceNum", invoice.num);
+            this.set("selectedInvoiceDueDate", invoice.dueDate);
             this.set("selectedInvoiceAmount", invoice.amount);
             this.set('isShowingModalDialog', true);
         },
         closeModalDialog() {
             this.set('isShowingModalDialog', false);
-        }
+        },
+        openDatePicker: function () {
+            // this.set("newDate", new Date().toISOString().slice(0, 10));
+        },
     }
 });
